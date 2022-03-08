6973109140b40c927c4b2fc7a906b2a81607a043
Emmie Ohnuki <emaohn@gmail.com>
2022-03-08 13:23:25 -0500

    Adjust spacings for Timeline and Timelines components

--- front-end/pages/dashboard/Timeline.js
+++ front-end/pages/dashboard/Timeline.js
@@ -4,6 +4,8 @@ import Grid from '@mui/material/Grid';
 import Box from '@mui/material/Grid';
 import Typography from '@mui/material/Typography';
 import {
+    ArgumentAxis,
+    ValueAxis,
     Chart,
     SplineSeries,
     Tooltip,
@@ -26,33 +28,31 @@ export default function Timeline(props) {
     const [hover, changeHover] = useState(null);
 
     return (
-        <Paper elevation={3}>
-            <Grid container direction='column' spacing={3}>
-                <Box m={5}>
-                    <Grid item>
-                        <Typography variant='subtitel1' gutterBottom component='div'>
-                            Total Logs
-                        </Typography>
-                        <Typography variant='h5' gutterBottom component='div'>
-                            25000
-                        </Typography>
-                    </Grid>
-                    <Grid item>
-                        <Chart
-                            data={data}
-                        >
-                            <SplineSeries valueField='logs' argumentField='time' />
-                            <EventTracker />
-                            <HoverState hover={hover} onHoverChange={changeHover} />
-                            <Tooltip
-                                visible={hover}
-                                closeOnOutsideClick={false}
-                            >
-                            </Tooltip>
-                        </Chart>
-                    </Grid>
-                </Box>
+        <Grid container direction='column'>
+            <Grid item>
+                <Typography variant='subtitel1' gutterBottom component='div'>
+                    {props.title}
+                </Typography>
+                <Typography variant='h5' gutterBottom component='div'>
+                    25000
+                </Typography>
             </Grid>
-        </Paper>
+            <Grid item>
+                <Chart
+                    data={data}
+                    height={100}
+                >
+                    <SplineSeries valueField='logs' argumentField='time' />
+                    <ArgumentAxis />
+                    <EventTracker />
+                    <HoverState hover={hover} onHoverChange={changeHover} />
+                    <Tooltip
+                        visible={hover}
+                        closeOnOutsideClick={false}
+                    >
+                    </Tooltip>
+                </Chart>
+            </Grid>
+        </Grid>
     )
 }

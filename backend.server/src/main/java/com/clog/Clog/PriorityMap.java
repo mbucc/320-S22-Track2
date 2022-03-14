3d6c4108f3d8ffbc6bcfb6732700c3ebd46f24a4
bmac400 <bryanmccaf@gmail.com>
2022-03-14 01:30:05 -0400

    Updated entry maps

--- backend.server/src/main/java/com/clog/Clog/PriorityMap.java
+++ backend.server/src/main/java/com/clog/Clog/PriorityMap.java
@@ -7,7 +7,9 @@ public class PriorityMap {
     private Map<String,Integer> priomap;
     public PriorityMap() {
         priomap = new HashMap<String,Integer>();
-        priomap.put("high",10);
+        priomap.put("low",10);
+        priomap.put("medium", 50);
+        priomap.put("high", 70);
     }
     public Integer get(String input){
         return priomap.get(input);
--- backend.server/src/main/java/com/clog/Clog/SeverityMap.java
+++ backend.server/src/main/java/com/clog/Clog/SeverityMap.java
@@ -1,12 +1,14 @@
 package com.clog.Clog;
 
 import java.util.HashMap;
-
+//Severity - Decode Severity as follows: Info >= 10 and < 30, Warning >= 30 and < 50, Error >= 50
 public class SeverityMap {
     private HashMap<String,int[]> severMap;
     public SeverityMap() {
         severMap = new HashMap<String,int[]>();
-        severMap.put("error", new int[]{0,50});
+        severMap.put("error", new int[]{50,Integer.MAX_VALUE});
+        severMap.put("info", new int[]{10,29});
+        severMap.put("info", new int[]{30,49});
         
     }
     public int[] getRange(String input) {

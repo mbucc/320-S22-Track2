16a7ceb2a52d32249d46932b4ec06e0e82e9a55c
Eric Lehmann <ericlehmann@Erics-MacBook-Pro.local>
2022-03-14 17:52:01 -0400

    created table component, added pagination

---
+++ front-end/Components/LogEvent-Form/LETable.js
@@ -0,0 +1,77 @@
+import React, {useState} from 'react'
+import { Table, TableHead, TableBody, TableRow, TableCell } from '@mui/material';
+import { TablePagination } from '@mui/material';
+import { InvertColors } from '@mui/icons-material';
+
+{/* LogEvent Table component with pagination. */}
+
+export default function LETable(props) {
+
+    const tableStyle = {
+        marginTop: "80px",
+    }
+
+    {/* states for the table */}
+    const [page, setPage] = useState(0)
+    const [rowsPerPage, setRPP] = useState(10)
+
+    {/* event handlers for changing number of pages and changing page*/}
+    const handleChangePage = (event, newPage) => {
+        setPage(newPage)
+    }
+    const handleChangeRPP = (event) => {
+        setRPP(parseInt(event.target.value, 10))
+        setPage(0)
+    }
+    
+
+  return (
+      <div style={tableStyle}>
+        <Table>
+            <TableHead >
+                <TableRow>
+                    <TableCell >Severity</TableCell> 
+                    <TableCell >Priority</TableCell>
+                    <TableCell >Category</TableCell>
+                    <TableCell >Created Date</TableCell>
+                    <TableCell >Application</TableCell>
+                    <TableCell >Process/Service</TableCell>
+                    <TableCell >Activity</TableCell>
+                    <TableCell >EAI Domain</TableCell>
+                    <TableCell >Log Event</TableCell>
+                </TableRow>
+            </TableHead>
+            <TableBody>
+                {props.data
+                .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+                .map((e, i)=>{
+                    return(
+                        <TableRow key = {i}>
+                            <TableCell>{e.severity}</TableCell>
+                            <TableCell>{e.priority}</TableCell>
+                            <TableCell>{e.category}</TableCell>
+                            <TableCell>{e["Created Date"]}</TableCell>
+                            <TableCell>{e["Application"]}</TableCell>
+                            <TableCell>{e["Process/Service"]}</TableCell>
+                            <TableCell>{e["Activity"]}</TableCell>
+                            <TableCell>{e["EAI Domain"]}</TableCell>
+                            <TableCell>{e["Log Event"]}</TableCell>
+                        </TableRow>
+                    )
+                })}
+            </TableBody>
+
+
+        </Table>
+        <TablePagination
+            count = {props.data.length}
+            rowsPerPageOptions = {[5, 10, 20, 50]}
+            page = {page}
+            onPageChange = {handleChangePage}
+            rowsPerPage = {rowsPerPage}
+            onRowsPerPageChange = {handleChangeRPP}
+            />
+    </div>
+
+  )
+}
--- front-end/pages/LogEvent.js
+++ front-end/pages/LogEvent.js
@@ -1,6 +1,6 @@
 import React, {useState} from 'react'
-import { Table, TableHead, TableBody, TableRow, TableCell } from '@mui/material';
 import Form from "../Components/LogEvent-Form/Form.js"
+import LETable from '../Components/LogEvent-Form/LETable.js';
 
 export default function LogEvent() {
     const mockData  = [
@@ -12,53 +12,23 @@ export default function LogEvent() {
         {"severity": "warning", "Created Date": "3/2/2022", "Process/Service": "Update Costumer", "Application": "CRM", "Activity": "Activity", "Log Event": "Detail", "priority": "medium", "category": "Status", "EAI Domain": "EAI Domain 1" },
         {"severity": "info", "Created Date": "3/2/2022", "Process/Service": "Update Costumer", "Application": "CRM", "Activity": "Activity", "Log Event": "Detail", "priority": "high", "category": "Security", "EAI Domain": "EAI Domain 1" },
         {"severity": "success", "Created Date": "3/2/2022", "Process/Service": "Update Costumer", "Application": "CRM", "Activity": "Activity", "Log Event": "Detail" , "priority": "low", "category": "Start", "EAI Domain": "EAI Domain 2"},
+        {"severity": "error", "Created Date": "3/10/2022", "Process/Service": "Update Costumer", "Application": "CRM", "Activity": "Activity", "Log Event": "Detail", "priority": "high", "category": "start", "EAI Domain": "EAI Domain 1" },
+        {"severity": "warning", "Created Date": "3/10/2022", "Process/Service": "Update Costumer", "Application": "CRM", "Activity": "Activity", "Log Event": "Detail", "priority": "low", "category": "Status", "EAI Domain": "EAI Domain 3" },
+        {"severity": "info", "Created Date": "3/10/2022", "Process/Service": "Update Costumer", "Application": "CRM", "Activity": "Activity", "Log Event": "Detail", "priority": "high" , "category": "Heartbeat", "EAI Domain": "EAI Domain 4"},
+        {"severity": "success", "Created Date": "3/10/2022", "Process/Service": "Update Costumer", "Application": "CRM", "Activity": "Activity", "Log Event": "Detail", "priority": "medium", "category": "Security", "EAI Domain": "EAI Domain 2"},
+        {"severity": "error", "Created Date": "3/10/2022", "Process/Service": "Update Costumer", "Application": "CRM", "Activity": "Activity", "Log Event": "Detail", "priority": "low" , "category": "Stop", "EAI Domain": "EAI Domain 3"},
+        {"severity": "warning", "Created Date": "3/10/2022", "Process/Service": "Update Costumer", "Application": "CRM", "Activity": "Activity", "Log Event": "Detail", "priority": "medium", "category": "Status", "EAI Domain": "EAI Domain 1" },
+        {"severity": "info", "Created Date": "3/10/2022", "Process/Service": "Update Costumer", "Application": "CRM", "Activity": "Activity", "Log Event": "Detail", "priority": "high", "category": "Security", "EAI Domain": "EAI Domain 1" },
+        {"severity": "success", "Created Date": "3/10/2022", "Process/Service": "Update Costumer", "Application": "CRM", "Activity": "Activity", "Log Event": "Detail" , "priority": "low", "category": "Start", "EAI Domain": "EAI Domain 2"},
     ]
 
     const [data, setData] = useState(mockData)
-    
-    const tableStyle = {
-        marginTop: "80px"
-    }
 
   return (
     <div>
         <h1>Log Events</h1>
         <Form mockData={mockData} setData={setData}/>
-        <Table style={tableStyle}>
-            <TableHead>
-                <TableRow>
-                    <TableCell>Severity</TableCell> 
-                    <TableCell>Priority</TableCell>
-                    <TableCell>Category</TableCell>
-                    <TableCell>Created Date</TableCell>
-                    <TableCell>Application</TableCell>
-                    <TableCell>Process/Service</TableCell>
-                    <TableCell>Activity</TableCell>
-                    <TableCell>EAI Domain</TableCell>
-                    <TableCell>Log Event</TableCell>
-                </TableRow>
-            </TableHead>
-            <TableBody>
-                {data.map((e, i)=>{
-                    return(
-                        <TableRow key = {i}>
-                          <TableCell>{e.severity}</TableCell>
-                          <TableCell>{e.priority}</TableCell>
-                          <TableCell>{e.category}</TableCell>
-                          <TableCell>{e["Created Date"]}</TableCell>
-                          <TableCell>{e["Application"]}</TableCell>
-                          <TableCell>{e["Process/Service"]}</TableCell>
-                          <TableCell>{e["Activity"]}</TableCell>
-                          <TableCell>{e["EAI Domain"]}</TableCell>
-                          <TableCell>{e["Log Event"]}</TableCell>
-                        </TableRow>
-                    )
-                })}
-            </TableBody>
-
-
-        </Table>
-        
+        <LETable data = {data}/>
     </div>
   )
 }

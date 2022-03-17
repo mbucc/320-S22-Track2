dbefdf1827c0f33886d12143059c36f852d33500
Lingxi Li <lilingxi01@gmail.com>
2022-03-16 23:26:05 -0400

    [Tree] Finish TreeFilter prototype.

--- front-end/components/business-process/tree/tree-filter.js
+++ front-end/components/business-process/tree/tree-filter.js
@@ -1,10 +1,14 @@
 import React, {useState} from 'react';
 import {BPDimens, BPStandards} from '../../../utils/business-process/standards';
-import {Button, TextField} from '@mui/material';
+import {Button} from '@mui/material';
 import BPTextInput from '../common/text-input';
+import {BPDatePicker} from '../common/date-picker';
+import {BPDomainSelector} from '../common/domain-selector';
+import {EAIDomainSample, PublishingBusinessDomainSample} from "../../../utils/business-process/sample-data";
 
 const BPTreeFilterComponent = ({onChange}) => {
   const [startDate, setStartDate] = useState(null);
+  const [endDate, setEndDate] = useState(null);
 
   return (
     <div
@@ -57,7 +61,7 @@ const BPTreeFilterComponent = ({onChange}) => {
           width: '100%',
           height: '100%',
           padding: 18,
-          overflowY: 'scroll',
+          overflowY: 'auto',
           overscrollBehaviorY: 'contain',
           flexShrink: 1,
           display: 'flex',
@@ -67,20 +71,24 @@ const BPTreeFilterComponent = ({onChange}) => {
           rowGap: 17,
         }}
       >
-        <BPTextInput
+        <BPDatePicker
           label={'Start Date'}
         />
 
-        <BPTextInput
+        <BPDatePicker
           label={'End Date'}
         />
 
-        <BPTextInput
+        <BPDomainSelector
           label={'EAI Domain'}
+          searchPlaceholder={'Search an EAI domain'}
+          list={EAIDomainSample}
         />
 
-        <BPTextInput
+        <BPDomainSelector
           label={'Publishing Business Domain'}
+          searchPlaceholder={'Search a publishing domain'}
+          list={PublishingBusinessDomainSample}
         />
       </div>
     </div>

1431cfe76855bd165970122619ee36d397b316fb
Lingxi Li <lilingxi01@gmail.com>
2022-03-17 09:51:32 -0400

    [Sample] Update sample data with appropriate types

--- front-end/utils/business-process/sample-data.js
+++ front-end/utils/business-process/sample-data.js
@@ -107,162 +107,45 @@ export const sampleEAIDomains = [
 ];
 
 export const EAIDomainSample = [
-  {
-    id: '1',
-    name: 'EAI Domain 1',
-  },
-  {
-    id: '2',
-    name: 'EAI Domain 2',
-  },
-  {
-    id: '3',
-    name: 'EAI Domain 3',
-  },
-  {
-    id: '4',
-    name: 'EAI Domain 4',
-  },
-  {
-    id: '5',
-    name: 'EAI Domain 5',
-  },
-  {
-    id: '6',
-    name: 'EAI Domain 6',
-  },
+  'EAI Domain 1',
+  'EAI Domain 2',
+  'EAI Domain 3',
 ];
 
 export const PublishingBusinessDomainSample = [
-  {
-    id: '1',
-    name: 'Publishing Business Domain 1',
-  },
-  {
-    id: '2',
-    name: 'Publishing Business Domain 2',
-  },
-  {
-    id: '3',
-    name: 'Publishing Business Domain 3',
-  },
-  {
-    id: '4',
-    name: 'Publishing Business Domain 4',
-  },
-  {
-    id: '5',
-    name: 'Publishing Business Domain 5',
-  },
-  {
-    id: '6',
-    name: 'Publishing Business Domain 6',
-  },
-  {
-    id: '7',
-    name: 'Publishing Business Domain 7',
-  },
-  {
-    id: '8',
-    name: 'Publishing Business Domain 8',
-  },
-  {
-    id: '9',
-    name: 'Publishing Business Domain 9',
-  },
-  {
-    id: '10',
-    name: 'Publishing Business Domain 10',
-  },
+  'Publishing Business Domain 1',
+  'Publishing Business Domain 2',
+  'Publishing Business Domain 3',
+  'Publishing Business Domain 4',
+  'Publishing Business Domain 5',
+  'Publishing Business Domain 6',
+  'Publishing Business Domain 7',
+  'Publishing Business Domain 8',
+  'Publishing Business Domain 9',
+  'Publishing Business Domain 10',
+  'Publishing Business Domain 11',
+  'Publishing Business Domain 12',
 ];
 
-export const BusinessProcessSample = [
-  {
-    id: '1',
-    name: 'Business Process 1001',
-  },
-  {
-    id: '2',
-    name: 'Business Process 1002',
-  },
-  {
-    id: '3',
-    name: 'Business Process 1003',
-  },
-  {
-    id: '4',
-    name: 'Business Process 1004',
-  },
-  {
-    id: '5',
-    name: 'Business Process 1005',
-  },
-  {
-    id: '6',
-    name: 'Business Process 1006',
-  },
-  {
-    id: '7',
-    name: 'Business Process 1007',
-  },
-  {
-    id: '8',
-    name: 'Business Process 1008',
-  },
-  {
-    id: '9',
-    name: 'Business Process 1009',
-  },
-  {
-    id: '10',
-    name: 'Business Process 1010',
-  },
-  {
-    id: '11',
-    name: 'Business Process 1011',
-  },
-  {
-    id: '12',
-    name: 'Business Process 1012',
-  },
-  {
-    id: '13',
-    name: 'Business Process 1013',
-  },
-  {
-    id: '14',
-    name: 'Business Process 1014',
-  },
-  {
-    id: '15',
-    name: 'Business Process 1015',
-  },
-  {
-    id: '16',
-    name: 'Business Process 1016',
-  },
-  {
-    id: '17',
-    name: 'Business Process 1017',
-  },
-  {
-    id: '18',
-    name: 'Business Process 1018',
-  },
-  {
-    id: '19',
-    name: 'Business Process 1019',
-  },
-  {
-    id: '20',
-    name: 'Business Process 1020',
-  },
-  {
-    id: '21',
-    name: 'Business Process 1021',
-  },
-  {
-    id: '22',
-    name: 'Business Process 1022',
-  },
+export const BusinessDomainSample = [
+  'Business Domain 1',
+  'Business Domain 2',
+  'Business Domain 3',
+  'Business Domain 4',
+  'Business Domain 5',
+  'Business Domain 6',
+  'Business Domain 7',
+  'Business Domain 8',
+  'Business Domain 9',
+  'Business Domain 10',
+  'Business Domain 11',
+  'Business Domain 12',
+  'Business Domain 13',
+  'Business Domain 14',
+  'Business Domain 15',
+  'Business Domain 16',
+  'Business Domain 17',
+  'Business Domain 18',
+  'Business Domain 19',
+  'Business Domain 20',
 ];

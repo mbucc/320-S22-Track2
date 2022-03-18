bc8e366d2a294c6a7ac4008ab94a08fd35d5ff45
dorepro <dore123222@gmail.com>
2022-03-18 04:15:14 -0400

    Added expand all, changed sample data

--- front-end/components/business-process/tree/tree-map.js
+++ front-end/components/business-process/tree/tree-map.js
@@ -7,76 +7,78 @@ import {sampleEAIDomains} from '../../../utils/business-process/sample-data';
 import {BPTextButton} from '../common/button';
 
 import BPTreeMapActivityEntry from './tree-map-item-activity';
+import ControlledTreeView from './tree-view';
 
 const BPTreeMapComponent = ({onChange}) => {
   return (
-    <div
-      style={{
-        width: '100%',
-        height: '100%',
-        display: 'flex',
-        flexDirection: 'column',
-      }}
-    >
-      {/* Top Bar */}
-      <div
-        style={{
-          width: '100%',
-          height: BPDimens.toolbarHeight,
-          flexShrink: 0,
-          flexGrow: 0,
-          padding: '0px 20px',
-          borderBottom: BPStandards.border,
-          backgroundColor: '#fff',
-          display: 'flex',
-          flexDirection: 'row',
-          alignItems: 'center',
-          justifyContent: 'flex-start',
-          columnGap: 2,
-        }}
-      >
-        <BPTextButton>Expand All</BPTextButton>
-        <BPTextButton>Collapse All</BPTextButton>
-      </div>
+    // <div
+    //   style={{
+    //     width: '100%',
+    //     height: '100%',
+    //     display: 'flex',
+    //     flexDirection: 'column',
+    //   }}
+    // >
+    //   {/* Top Bar */}
+    //   <div
+    //     style={{
+    //       width: '100%',
+    //       height: BPDimens.toolbarHeight,
+    //       flexShrink: 0,
+    //       flexGrow: 0,
+    //       padding: '0px 20px',
+    //       borderBottom: BPStandards.border,
+    //       backgroundColor: '#fff',
+    //       display: 'flex',
+    //       flexDirection: 'row',
+    //       alignItems: 'center',
+    //       justifyContent: 'flex-start',
+    //       columnGap: 2,
+    //     }}
+    //   >
+    //     <BPTextButton>Expand All</BPTextButton>
+    //     <BPTextButton>Collapse All</BPTextButton>
+    //   </div>
 
-      {/* Tree Map */}
-      <div
-        style={{
-          width: '100%',
-          height: '100%',
-          flexShrink: 1,
-          flexGrow: 0,
-          display: 'flex',
-          flexDirection: 'column',
-          alignItems: 'center',
-          justifyContent: 'flex-start',
-          padding: 20,
-          overflowY: 'scroll',
-          rowGap: 14,
-        }}
-      >
-        {
-          /* The main map hierarchy */
-          sampleEAIDomains.map((eaiDomain) => {
-            return (
-              <BPTreeMapEAIDomain domain={eaiDomain} key={eaiDomain.name}>
-                {(publishingDomains) => publishingDomains.map((pubDomain) => (
-                  <BPTreeMapPublishingDomain domain={pubDomain} key={pubDomain.name}>
-                    {(businessProcesses) => businessProcesses.map((process) => (
-                      <BPTreeMapBusinessProcess process={process} key={process.name}>
-                        {(activities) => activities.map((activity) => (
-                          <BPTreeMapActivityEntry activity={activity} key={activity.id}/>
-                        ))}
-                      </BPTreeMapBusinessProcess>
-                    ))}
-                  </BPTreeMapPublishingDomain>
-                ))}
-              </BPTreeMapEAIDomain>
-            );
-          })
-        }
-      </div>
-    </div>
+    //   {/* Tree Map */}
+    //   <div
+    //     style={{
+    //       width: '100%',
+    //       height: '100%',
+    //       flexShrink: 1,
+    //       flexGrow: 0,
+    //       display: 'flex',
+    //       flexDirection: 'column',
+    //       alignItems: 'center',
+    //       justifyContent: 'flex-start',
+    //       padding: 20,
+    //       overflowY: 'scroll',
+    //       rowGap: 14,
+    //     }}
+    //   >
+    //     {
+    //       /* The main map hierarchy */
+    //       sampleEAIDomains.map((eaiDomain) => {
+    //         return (
+    //           <BPTreeMapEAIDomain domain={eaiDomain} key={eaiDomain.name}>
+    //             {(publishingDomains) => publishingDomains.map((pubDomain) => (
+    //               <BPTreeMapPublishingDomain domain={pubDomain} key={pubDomain.name}>
+    //                 {(businessProcesses) => businessProcesses.map((process) => (
+    //                   <BPTreeMapBusinessProcess process={process} key={process.name}>
+    //                     {(activities) => activities.map((activity) => (
+    //                       <BPTreeMapActivityEntry activity={activity} key={activity.id}/>
+    //                     ))}
+    //                   </BPTreeMapBusinessProcess>
+    //                 ))}
+    //               </BPTreeMapPublishingDomain>
+    //             ))}
+    //           </BPTreeMapEAIDomain>
+    //         );
+    //       })
+    //     }
+    //   </div>
+    // </div>
+    <ControlledTreeView/>
   );
 };
 

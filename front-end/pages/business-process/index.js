28f803c33de9b7ecb3b230ed766f974ab8e8d922
efathalizade <efathalizade@umass.edu>
2022-03-23 13:51:49 -0400

    basic adjustable frame

--- front-end/pages/business-process/index.js
+++ front-end/pages/business-process/index.js
@@ -2,35 +2,29 @@ import Head from 'next/head';
 import BPTreeView from '../../components/business-process/tree/core';
 import BPActivityView from '../../components/business-process/activity/core';
 import {BPColors, BPStandards} from '../../utils/business-process/standards';
+import Split from './split';
+//import './style.css';
+
 
 
 const BPIndex = () => {
   return (
-    <div
+     
+    <div 
       style={{
-        width: '100%',
-        height: '100vh', // 100vh = 100% of viewport height.
-      }}
+      width: '100%',
+      height: '100vh', // 100vh = 100% of viewport height.
+    }}
     >
       <Head>
         <title>Business Process - CLOG</title>
       </Head>
 
-      {/* The core layout */}
       <div
-        style={{
-          width: '100%',
-          height: '100%',
-          display: 'flex',
-          flexDirection: 'column',
-          alignItems: 'center',
-          justifyContent: 'center',
-        }}
-      >
-        <div
           style={{
             width: '100%',
-            height: 70,
+            //height: 70,
+            height: '8%',
             flexShrink: 0,
             backgroundColor: '#fbfbfb',
             borderBottom: BPStandards.border,
@@ -43,43 +37,22 @@ const BPIndex = () => {
             Navigation Placeholder
           </p>
         </div>
-        <div
-          style={{
-            width: '100%',
-            height: '55%',
-            maxHeight: 'calc(55vh - 37px)',
-            flexShrink: 1,
-            flexGrow: 0,
-            backgroundColor: '#ffffff',
-          }}
-        >
-          <BPTreeView />
-        </div>
 
-        {/* RootView Divider */}
-        <div
-          style={{
-            width: '100%',
-            height: 4,
-            flexShrink: 0,
-            backgroundColor: BPColors.border,
-          }}
-        />
-
-        <div
-          style={{
-            width: '100%',
-            height: '45%',
-            maxHeight: 'calc(45vh - 37px)',
-            flexShrink: 1,
-            flexGrow: 0,
-            backgroundColor: '#ffffff',
-          }}
-        >
-          <BPActivityView />
-        </div>
+      <div 
+        style={{
+          width: '100%',
+          //height: '100%',
+          height: '92%',
+          
+        }}
+      >
+        <Split />
+        
       </div>
+      
     </div>
+     
+     
   );
 };
 

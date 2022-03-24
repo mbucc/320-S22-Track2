d6ddb3f7375416ab42271376a0f5294ea2c90fb0
bmac400 <bryanmccaf@gmail.com>
2022-03-24 14:34:04 -0400

    Removed Unused inputs

--- backend.server/src/main/java/com/clog/Clog/MainController.java
+++ backend.server/src/main/java/com/clog/Clog/MainController.java
@@ -1,6 +1,6 @@
 package com.clog.Clog;
 
-import java.util.HashMap;
+
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
@@ -23,15 +23,11 @@ import com.clog.Clog.LogEventFiles.LogEventsSearchCriteria;
 
 import java.sql.Timestamp;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
-
-import ch.qos.logback.core.filter.Filter;
-
 
 @Controller
 @RequestMapping(path="/clog")

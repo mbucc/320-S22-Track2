1011b8b719da62c5094e98bdd3af28b3f2d18b36
Emmie Ohnuki <emaohn@gmail.com>
2022-03-26 14:16:58 -0400

    add useStates and conditional rendering

--- front-end/pages/index.js
+++ front-end/pages/index.js
@@ -1,16 +1,37 @@
-import Head from 'next/head';
-import Image from 'next/image';
-import styles from '../styles/Home.module.css';
-
-import {TextField} from '@mui/material';
 import Dashboard from './dashboard/Dashboard'
+import React, { useState } from 'react'
 
 /**
  * The home page of the website.
  * @return {JSX.Element}
  */
 export default function Home() {
+  const [loggedIn, setLogin] = useState(true)
+  const [display, setDisplay] = useState(0) //0, 1, 2 (dashboard, business process, log events)   
+  const [bpFilters, setbpFilters] = useState(null)
+  const [logeventFilters, setlogeventFilters] = useState(null)
+
+  const [value, setValue] = React.useState(0);
+
+  const toggleNav = (event, tab) => {
+    setDisplay(tab);
+  };
+
+
+  if (loggedIn) {
+    return (
+      <div>
+        {/* <NavBar onChange={toggleNav} />*/}
+        {display == 0
+          ? <Dashboard />
+          : display == 1
+            ? <div>Business Process</div>
+            : <div>Log Events</div>
+        }
+      </div>
+    )
+  }
   return (
-    <Dashboard />
-  );
+    <div>Login</div>
+  )
 }

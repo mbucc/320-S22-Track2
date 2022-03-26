c484f19e75ce600508220244219b15d0abeebf73
Lingxi Li <lilingxi01@gmail.com>
2022-03-26 13:09:03 -0400

    [Table] Build a really basic prototype of ActivityTable.
    No any data or columns set up yet.

--- front-end/components/business-process/activity/activity-table.js
+++ front-end/components/business-process/activity/activity-table.js
@@ -1,14 +1,210 @@
-import React from 'react';
+import React, {useEffect, useMemo, useState} from 'react';
+
+// WebStorm doesn't understand ES6 imports and throw a warning of "cannot resolve 'useTable'". It seems working.
+import {useTable, useBlockLayout, useResizeColumns} from 'react-table';
+
+import styled from 'styled-components';
+import {BPColors, BPDimens, BPStandards} from '../../../utils/business-process/standards';
 
 /**
- * @class ActivityTable
- * @return {JSX.Element}
+ * The root component for the activity table.
  */
-export default function BPActivityTableComponent({}) {
-  // TODO.
+const BPTableRootStructure = styled.div`
+  width: 100%;
+  height: 100%;
+  overflow-x: auto;
+  overflow-y: auto;
+
+  .table {
+    width: 100%;
+    
+    display: inline-block;
+    border-spacing: 0;
+    border: 0;
+    
+    .table-header {
+      height: ${BPDimens.toolbarHeight}px;
+      border-bottom: ${BPStandards.border};
+      padding-left: 12px;
+      padding-right: 12px;
+      display: flex;
+      align-items: center;
+      justify-content: flex-start;
+      font-size: 17px;
+      font-weight: 500;
+    }
+
+    .table-body {
+      padding-bottom: 40px;
+      padding-left: 12px;
+      padding-right: 12px;
+    }
+    
+    .table-line {
+      width: 100%;
+      display: flex;
+      flex-direction: column;
+      align-items: flex-start;
+      justify-content: center;
+
+      &:hover {
+        background-color: ${BPColors.gray[100]};
+        border-radius: ${BPDimens.treeRadius};
+      }
+    }
+
+    .table-line-divider {
+      width: calc(100% - 20px);
+      height: 1px;
+      margin: 0 10px;
+      background-color: ${BPColors.gray[100]};
+      opacity: 90%;
+    }
+
+    .th,
+    .td {
+      margin: 0;
+      padding: 14px 14px;
+
+      // In this example we use an absolutely position resizer, so this is required.
+      position: relative;
+
+      :last-child {
+        border-right: 0;
+      }
+
+      .resizer {
+        display: inline-block;
+        background: ${BPColors.gray[200]};
+        width: 2.5px;
+        height: 16.5px;
+        border-radius: 999px;
+        position: absolute;
+        right: 0;
+        top: 50%;
+        transform: translateY(-50%);
+        transition: all 0.15s ease-in-out;
+        z-index: 1;
+        // Prevents from scrolling while dragging on touch devices.
+        touch-action: none;
+        
+        &:hover {
+          background: ${BPColors.gray[500]};
+        }
+
+        &.isResizing {
+          background: ${BPColors.gray[800]};
+        }
+      }
+    }
+  }
+`;
+
+/**
+ * This is a custom implementation of the react-table `useTable` hook.
+ * @param {array} columns
+ * @param {array} data
+ * @return {JSX.Element}
+ * @constructor
+ */
+export default function BPTableComponent({columns, data}) {
+  const [hasMounted, setHasMounted] = useState(false);
+
+  useEffect(() => {
+    setHasMounted(true);
+  }, []);
+
+  // The default column width configuration.
+  const defaultColumnConfiguration = useMemo(
+      () => ({
+        minWidth: 130,
+        maxWidth: 700,
+      }),
+      []
+  );
+
+  const {
+    getTableProps,
+    getTableBodyProps,
+    headerGroups,
+    rows,
+    prepareRow,
+  } = useTable(
+      {
+        columns,
+        data,
+        defaultColumn: defaultColumnConfiguration,
+        autoResetResize: false,
+      },
+      useBlockLayout,
+      useResizeColumns
+  );
+
+  // Rehydrate the table right when the component mounts.
+  if (!hasMounted) {
+    return null;
+  }
+
+  // The main table structure.
   return (
-    <div>
-      <p>Table Section</p>
-    </div>
+    <BPTableRootStructure>
+      <div {...getTableProps()} className="table">
+        <div
+          className="table-header"
+          style={{
+            position: 'sticky',
+            top: 0,
+            zIndex: 9999,
+            background: BPColors.white,
+          }}
+        >
+          {headerGroups.map((headerGroup) => (
+            // eslint-disable-next-line react/jsx-key
+            <div {...headerGroup.getHeaderGroupProps()} className="tr">
+              {headerGroup.headers.map((column) => (
+                // eslint-disable-next-line react/jsx-key
+                <div {...column.getHeaderProps()} className="th">
+                  {column.render('Header')}
+                  {/* Use column.getResizerProps to hook up the events correctly */}
+                  <div
+                    {...column.getResizerProps()}
+                    className={`resizer ${
+                      column.isResizing ? 'isResizing' : ''
+                    }`}
+                  />
+                </div>
+              ))}
+            </div>
+          ))}
+        </div>
+
+        <div {...getTableBodyProps()} className="table-body">
+          {rows.map((row, i) => {
+            prepareRow(row);
+            return (
+              // eslint-disable-next-line react/jsx-key
+              <div key={i} className="table-line">
+                <div {...row.getRowProps()} className="tr">
+                  {row.cells.map((cell) => {
+                    return (
+                      // eslint-disable-next-line react/jsx-key
+                      <div {...cell.getCellProps()} className="td">
+                        {cell.render('Cell')}
+                      </div>
+                    );
+                  })}
+                </div>
+                <div
+                  className="table-line-divider"
+                  style={{
+                    display: i === rows.length - 1 ? 'none' : 'flex',
+                  }}
+                />
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    </BPTableRootStructure>
   );
 }
--- front-end/components/business-process/activity/core.js
+++ front-end/components/business-process/activity/core.js
@@ -1,11 +1,98 @@
 import React from 'react';
 import {BPColors} from '../../../utils/business-process/standards';
 import BPActivityFilterComponent from './activity-filter';
-import BPActivityTableComponent from './activity-table';
+import BPTableComponent from './activity-table';
+import {
+  BPActivitySeverityIcon,
+  getColorBySeverity,
+  getNameBySeverityAccessor
+} from '../../../utils/business-process/severity';
 
 const BPActivityView = ({
   onChange,
 }) => {
+  const columns = React.useMemo(
+      () => [
+        {
+          Header: 'Severity',
+          accessor: 'severity',
+          width: 140,
+          Cell: ({row}) => {
+            const data = row.original;
+            return (
+              <div
+                style={{
+                  display: 'flex',
+                  flexDirection: 'row',
+                  alignItems: 'center',
+                  justifyContent: 'flex-start',
+                  columnGap: '0.5rem',
+                  fontSize: '1.0rem',
+                  fontWeight: '500',
+                  color: getColorBySeverity(data.severity),
+                }}
+              >
+                <BPActivitySeverityIcon severity={data.severity}/>
+                {getNameBySeverityAccessor(data.severity)}
+              </div>
+            );
+          },
+        },
+        {
+          Header: 'Info',
+          accessor: 'info',
+          width: 600,
+        },
+      ],
+      []
+  );
+
+  const data = React.useMemo(
+      () => [
+        {
+          severity: 'info',
+          info: 'This is the first activity',
+        },
+        {
+          severity: 'warning',
+          info: 'This is the second activity',
+        },
+        {
+          severity: 'error',
+          info: 'This is the third activity',
+        },
+        {
+          severity: 'info',
+          info: 'This is the fourth activity',
+        },
+        {
+          severity: 'info',
+          info: 'This is the fifth activity',
+        },
+        {
+          severity: 'success',
+          info: 'This is the sixth activity',
+        },
+        {
+          severity: 'error',
+          info: 'This is the seventh activity',
+        },
+        {
+          severity: 'error',
+          info: 'This is the eighth activity',
+        },
+        {
+          severity: 'success',
+          info: 'This is the ninth activity',
+        },
+        {
+          severity: 'warning',
+          info: 'This is the tenth activity',
+        },
+      ],
+      []
+  );
+
   return (
     <div
       style={{
@@ -34,13 +121,14 @@ const BPActivityView = ({
           height: '100%',
           flexShrink: 1,
           flexGrow: 0,
+          overflowX: 'auto',
           backgroundColor: '#ffffff',
           display: 'flex',
           alignItems: 'center',
           justifyContent: 'center',
         }}
       >
-        <BPActivityTableComponent/>
+        <BPTableComponent columns={columns} data={data}/>
       </div>
     </div>
   );

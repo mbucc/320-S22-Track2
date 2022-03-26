f16a59163af024cde2dcf63dcfe334a19a0c6cfa
Emmie Ohnuki <emaohn@gmail.com>
2022-03-26 15:09:13 -0400

    Add navbar and functions for navbar toggling

--- front-end/pages/index.js
+++ front-end/pages/index.js
@@ -1,5 +1,7 @@
 import React, { useState } from 'react'
 import Dashboard from './dashboard/Dashboard';
+import Navbar from './dashboard/Navbar';
+import Grid from '@mui/material/Grid';
 
 /**
  * The home page of the website.
@@ -8,27 +10,48 @@ import Dashboard from './dashboard/Dashboard';
 export default function Home() {
   const [loggedIn, setLogin] = useState(true)
   const [display, setDisplay] = useState(0) //0, 1, 2 (dashboard, business process, log events)   
-  const [bpFilters, setbpFilters] = useState(null)
-  const [logeventFilters, setlogeventFilters] = useState(null)
+  const bpFilters = null
+  const logeventFilters = null
 
-  const [value, setValue] = React.useState(0);
-
+  // function to pass into Navbar to handle tab clicking and changing the displayed view
   const toggleNav = (event, tab) => {
     setDisplay(tab);
   };
 
+  /*
+  * Function to be called by clickable components in Dashboard
+  * Requires relevant filters for business process views
+  * Sets display to business process and passes filters into component view
+  */
+  const toggleBP = (filters) => {
+    
+  }
+
+  /*
+  * Function to be called by clickable components in Dashboard
+  * Requires relevant filters for log events views
+  * Sets display to log events and passes filters into component view
+  */
+  const toggleLogEvents = (filters) => {
+
+  }
 
   if (loggedIn) {
     return (
-      <div>
+      <Grid container direction='column'>
+        <Grid item height={'100%'}>
+          <Navbar />
+        </Grid>
         {/* <NavBar onChange={toggleNav} />*/}
-        {display == 0
-          ? <Dashboard />
-          : display == 1
-            ? <div>Business Process</div>
-            : <div>Log Events</div>
-        }
-      </div>
+        <Grid item height={'100%'}>
+          {display == 0
+            ? <Dashboard onBPClick={toggleBP} onClick={toggleLogEvents} />
+            : display == 1
+              ? <div>Business Process</div>
+              : <div>Log Events</div>
+          }
+        </Grid>
+      </Grid>
     )
   }
   return (

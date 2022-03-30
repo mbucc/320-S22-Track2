173c3af264687e5649494fb7e462dacccdb60c1e
andrew mead <ammead@umass.edu>
2022-03-29 22:37:34 -0400

    Remove vuln

--- backend.server/src/utils/create_data.py
+++ backend.server/src/utils/create_data.py
@@ -2,7 +2,7 @@ import pymysql
 
 host = 'track2-clog-master.c0pfgcylcs5z.us-east-1.rds.amazonaws.com'
 user = 'thecafebabe'
-password = 'idn3XtSV6McnzU'
+password = ''
 database = 'clog'
 
 connection = pymysql.connect(
@@ -16,22 +16,18 @@ global_instance_id = 0
 business_domain = ['CRM', 'OPER', 'ACCOUNT']
 business_subdomain = ['CUSTOMER', 'GRID', 'UPDATE']
 version = 1.0
-local_instance_id = []  # some combo of busness_domanins
-eai_transaction_id = []
-eai_domain = []
-hostname = []
-application = []
-event_context = []
-component = []
-severity = []
-priority = []
+local_instance_id = ''  # some combo of busness_domanins and id
+eai_transaction_id = ['server-1', 'computer-5', 'embeded-system-23']
+eai_domain = ['EAI_DOMAIN_1', 'EAI_DOMAIN_2', 'EAI_DOMAIN_3', 'EAI_DOMAIN_4']
+hostname = ['crm_server', 'acct_server', 'oper_server']
+application = []  # business subdomain _ application
+event_context = ['UPDATE', 'INFO']
+component = ['UPDATE', 'INFO']
+severity = [0, 9, 10, 20, 29, 30, 40, 49, 50, 60]
+priority = [10, 50, 70]
 creation_time = []
-reasoning_scope = []
+reasoning_scope = ['INTERNAL', 'EXTERNAL']
 process_id = []
 category_name = []
 activity = []
 msg = []
-
-for row in cursor:
-    business_domain.append(row[1])
-    business_subdomain.append(row[2])

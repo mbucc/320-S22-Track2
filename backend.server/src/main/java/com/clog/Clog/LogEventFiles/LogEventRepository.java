2b241374e1e5a3dcecb774cd93dd657c4aa6d05e
James <duyquangnguyenhac@gmail.com>
2022-04-05 20:13:25 -0400

    Minor changes to tests

--- backend.server/src/main/java/com/clog/Clog/LogEventFiles/LogEventRepository.java
+++ backend.server/src/main/java/com/clog/Clog/LogEventFiles/LogEventRepository.java
@@ -1,9 +1,12 @@
 package com.clog.Clog.LogEventFiles;
+
+import java.util.List;
+
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.lang.Nullable;
 
-
-public interface LogEventRepository extends JpaRepository<LogEvent, String>, JpaSpecificationExecutor<LogEvent>{
-
-} 
-
+public interface LogEventRepository extends JpaRepository<LogEvent, String>, JpaSpecificationExecutor<LogEvent> {
+    List<LogEvent> findAll(@Nullable Specification<LogEvent> spec);
+}

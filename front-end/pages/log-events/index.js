1d00e5886903e32de94a8b4e2ea4c1798efb8949
syuen23 <simonyuenjr@outlook.com>
2022-04-07 12:38:33 -0400

    created index.js for a temp log events page

---
+++ front-end/pages/log-events/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Navbar from '../dashboard/Navbar';
+
+function Home() {
+    return (
+        <>
+            <Navbar />
+            <div>
+                Log Events
+            </div>
+        </>
+    );
+}
+
+export default Home;

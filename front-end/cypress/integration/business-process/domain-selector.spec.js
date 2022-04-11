aaf9bc5b20c1043b17e29849e98f31e2b8e946be
TornadoOfSoup <senseipower@gmail.com>
2022-04-11 19:53:11 -0400

    Modified domain-selector.spec.js, not finished yet

--- front-end/cypress/integration/business-process/domain-selector.spec.js
+++ front-end/cypress/integration/business-process/domain-selector.spec.js
@@ -1,1 +1,37 @@
+import { isValidTimeFormat } from "../../../utils/business-process/date-options";
+
 // TODO: Write tests for domain selectors.
+before(() => {
+    cy.visit('/business-process');
+  });
+
+  describe('EAI domain is working properly.', () => {
+    it('EAI domain field is loaded', () => {
+        cy.get('#bp-tree-filter-eai-domain-selector').should('exist');
+        cy.get('#bp-tree-filter-eai-domain-selector-popper-list').should('not.exist');
+    })
+
+    it('The dropdown menu appears when the EAI domain field is clicked, and disappears if a click occurs elsewhere.', () => {
+        cy.get('#bp-tree-filter-eai-domain-selector').click();
+        cy.get('#bp-tree-filter-eai-domain-selector-popper-list').should('exist');
+        cy.get('body').click(0, 0);
+        cy.get('#bp-tree-filter-eai-domain-selector-popper-list').should('not.exist');
+    })
+
+    let entry = "";
+
+    it('Selecting an entry in the list displays it in the field.', () => {
+        cy.get('#bp-tree-filter-eai-domain-selector').click();
+        cy.get('#bp-tree-filter-eai-domain-selector-popper-list > :nth-child(2)').invoke('val').then(val => {
+            entry = val;
+        })
+        cy.get('#bp-tree-filter-eai-domain-selector-popper-list > :nth-child(2)').click();
+        cy.get('body').click(0, 0);
+        cy.get('#bp-tree-filter-eai-domain-selector').should('have.value', entry);
+    })
+
+    it('If something is typed into the search bar, all entries will contain the searched keyword.', () => {
+
+    })
+    
+  })

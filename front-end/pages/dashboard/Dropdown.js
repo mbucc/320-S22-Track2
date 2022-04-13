0c2e31d27409f0fab33e284455c04e7ba7a1263d
Michael Crummet <mcrummet@umass.edu>
2022-04-13 14:12:30 -0400

    Adjusted Dropown/Refresh Button

--- front-end/pages/dashboard/Dropdown.js
+++ front-end/pages/dashboard/Dropdown.js
@@ -7,15 +7,15 @@ export default function Dropdown(props) {
   return (
     <div className="Dropdown">
       <Select defaultValue={60}>
-        <MenuItem value = {15} onClick={function() { props.setTimeframe(15); props.refresh(); }}>15 mins</MenuItem>
-        <MenuItem value = {30} onClick={function() { props.setTimeframe(30); props.refresh(); }}>30 mins</MenuItem>
-        <MenuItem value = {60} onClick={function() { props.setTimeframe(60); props.refresh(); }}>1 hour</MenuItem>
-        <MenuItem value = {240} onClick={function() { props.setTimeframe(240); props.refresh(); }}>4 hours</MenuItem>
-        <MenuItem value = {720} onClick={function() { props.setTimeframe(720); props.refresh(); }}>12 hours</MenuItem>
-        <MenuItem value = {1440} onClick={function() { props.setTimeframe(1440); props.refresh(); }}>1 day</MenuItem>
+        <MenuItem value = {15} onClick={() => props.setTimeframe(15)}>15 mins</MenuItem>
+        <MenuItem value = {30} onClick={() => props.setTimeframe(30)}>30 mins</MenuItem>
+        <MenuItem value = {60} onClick={() => props.setTimeframe(60)}>1 hour</MenuItem>
+        <MenuItem value = {240} onClick={() => props.setTimeframe(240)}>4 hours</MenuItem>
+        <MenuItem value = {720} onClick={() => props.setTimeframe(720)}>12 hours</MenuItem>
+        <MenuItem value = {1440} onClick={() => props.setTimeframe(1440)}>1 day</MenuItem>
       </Select>
 
-      <IconButton color="primary" onClick={() => props.refresh()}>
+      <IconButton color="primary" onClick={() => props.setTimeframe(props.timeframe)}>
         <RefreshIcon color="action"></RefreshIcon>
       </IconButton>
     </div>

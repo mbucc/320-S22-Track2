97509c5487f3ee51cfbf53dc337061a6b982c55d
srinaldi-umass <srinaldi@umass.edu>
2022-04-13 14:16:15 -0400

    added more tests for the publishing business domain, still working on some tests though

--- front-end/cypress/integration/business-process/domain-selector.spec.js
+++ front-end/cypress/integration/business-process/domain-selector.spec.js
@@ -5,7 +5,11 @@ before(() => {
     cy.visit('/business-process');
   });
 
-  describe('EAI domain is working properly.', () => {
+  describe('EAI domain field is working properly.', () => {
+    it('Click at (0, 0) to reset pop-ups', () => {
+        cy.get('body').click(0, 0);
+    })
+
     it('EAI domain field is loaded', () => {
         cy.get('#bp-tree-filter-eai-domain-selector').should('exist');
         cy.get('#bp-tree-filter-eai-domain-selector-popper-list').should('not.exist');
@@ -25,8 +29,53 @@ before(() => {
         cy.get('#bp-tree-filter-eai-domain-selector-popper-list > :nth-child(2)').invoke('text').then(firstEntry => {
             cy.get('#bp-tree-filter-eai-domain-selector-popper-list > :nth-child(2)').click();
             cy.get('body').click(0, 0);
-            cy.get('#bp-tree-filter-eai-domain-selector').invoke('val').should((selectorVal) => {
-                expect(entry).to.eq(selectorVal);
+            cy.get('#bp-tree-filter-eai-domain-selector-selected-items').children().its(0).invoke('text').should((selectedVal) => {
+                expect(firstEntry).to.eq(selectedVal);
+            });
+            
+        })
+        
+    })
+
+    it('If something is typed into the search bar, all entries will contain the searched keyword.', () => {
+        cy.get('#bp-tree-filter-eai-domain-selector').click();
+        cy.get('#bp-tree-filter-eai-domain-selector-popper-list > :nth-child(2)').invoke('text').then(query => {
+            cy.get('#bp-tree-filter-eai-domain-selector-popper-search-field').type(query.substring(0, 1));
+            cy.get('#bp-tree-filter-eai-domain-selector-popper-list').each((val, index, collection) => {
+                //check that each result contains the substring
+            })
+        })
+        
+    })
+    
+  })
+
+  describe('Publishing Business domain field is working properly.', () => {
+    it('Click at (0, 0) to reset pop-ups', () => {
+        cy.get('body').click(0, 0);
+    })
+    
+    it('Publishing Business domain field is loaded', () => {
+        cy.get('#bp-tree-filter-publishing-business-domain-selector').should('exist');
+        cy.get('#bp-tree-filter-publishing-business-domain-selector-popper-list').should('not.exist');
+    })
+
+    it('The dropdown menu appears when the EAI domain field is clicked, and disappears if a click occurs elsewhere.', () => {
+        cy.get('#bp-tree-filter-publishing-business-domain-selector').click();
+        cy.get('#bp-tree-filter-publishing-business-domain-selector-popper-list').should('exist');
+        cy.get('body').click(0, 0);
+        cy.get('#bp-tree-filter-publishing-business-domain-selector-popper-list').should('not.exist');
+    })
+
+    let entry = "";
+
+    it('Selecting an entry in the list displays it in the field.', () => {
+        cy.get('#bp-tree-filter-publishing-business-domain-selector').click();
+        cy.get('#bp-tree-filter-publishing-business-domain-selector-popper-list > :nth-child(2)').invoke('text').then(firstEntry => {
+            cy.get('#bp-tree-filter-publishing-business-domain-selector-popper-list > :nth-child(2)').click();
+            cy.get('body').click(0, 0);
+            cy.get('#bp-tree-filter-publishing-business-domain-selector-selected-items').children().its(0).invoke('text').should((selectedVal) => {
+                expect(firstEntry).to.eq(selectedVal);
             });
             
         })
@@ -38,3 +87,4 @@ before(() => {
     })
     
   })
+

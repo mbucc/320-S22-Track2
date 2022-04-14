0aa2443428b6694a0cb58c7910984cb39b4b8886
animeshsaxen <animeshsaxen@umass.edu>
2022-04-14 00:05:20 -0400

    Updated test for new url

--- front-end/cypress/integration/Dashboard/graph.spec.js
+++ front-end/cypress/integration/Dashboard/graph.spec.js
@@ -6,6 +6,6 @@ before(() => {
 describe('Line Graph Test', () => {
     it('Checks if see more working properly', () => {
         cy.get('.MuiButton-root').eq(4).click()
-        cy.url().should('eq', "http://localhost:3000/log-events")
+        cy.url().should('eq', "http://localhost:3000/LogEvent")
     })
   })

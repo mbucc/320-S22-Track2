55ad1353376a96ba3677486279cd391664bf75ab
Aishwarya Rajapur <arjun.aishwarya@gmail.com>
2022-04-17 17:49:27 -0400

    updated population of table and basic sorting

--- front-end/cypress/integration/business-process/activity-table.spec.js
+++ front-end/cypress/integration/business-process/activity-table.spec.js
@@ -1,15 +1,16 @@
 // TODO: Write tests for activity table.
 
-describe("Activity Table should be able to be populated by clicking tree components", () => {
+describe("Activity Table Behavior", () => {
     beforeEach(() => {
         cy.visit("/business-process");
     });    
-    it("Populating activity table by clicking the tree", () => {
-        cy.get('#mui-1-EAI\ Domain\ 1 > .css-1g86id8-MuiTreeItem-content > .MuiTreeItem-label').should('exist');
+    //checks if clicking component in tree corresponds to the activity showing up in the activity table
+    it("Populating activity table by clicking the tree", () => { 
+        cy.get('#mui-1-EAI\\ Domain\\ 1 > .css-1g86id8-MuiTreeItem-content > .MuiTreeItem-label').click();
 
-        cy.get('#mui-1-Publishing\ Business\ Domain\ 1\ from\ EAI\ 1 > .Mui-expanded > .MuiTreeItem-label').click();
+        cy.get('#mui-1-Publishing\\ Business\\ Domain\\ 1\\ from\\ EAI\\ 1 > .css-1g86id8-MuiTreeItem-content > .MuiTreeItem-label').click();
 
-        cy.get('#mui-1-Business\ Process\ 1001 > .Mui-expanded > .MuiTreeItem-label').click();
+        cy.get('#mui-1-Business\\ Process\\ 1001 > .css-1g86id8-MuiTreeItem-content > .MuiTreeItem-label').click();
 
         cy.get('#mui-1-1 > .css-1g86id8-MuiTreeItem-content > .MuiTreeItem-label').click();
 
@@ -17,8 +18,21 @@ describe("Activity Table should be able to be populated by clicking tree compone
 
     });
     
-    it("Severity filter test: able to click on it", () => {
-        cy.get("#table-sorter").should("be.visible");
+    it("Sort activities in ascending order (severities should be first)", () => {
+        cy.get('#mui-1-EAI\\ Domain\\ 1 > .css-1g86id8-MuiTreeItem-content > .MuiTreeItem-label').click();
+
+        cy.get('#mui-1-Publishing\\ Business\\ Domain\\ 1\\ from\\ EAI\\ 1 > .css-1g86id8-MuiTreeItem-content > .MuiTreeItem-label').click();
+
+        cy.get('#mui-1-Business\\ Process\\ 1001 > .css-1g86id8-MuiTreeItem-content > .MuiTreeItem-label').click();
+
+        cy.get('#mui-1-1 > .css-1g86id8-MuiTreeItem-content > .MuiTreeItem-label').click(); 
+
+        cy.get('[style="display: inline-block; box-sizing: border-box; width: 140px; position: relative;"] > .table-header-sorter').click();
+
+        cy.get(':nth-child(1) > .tr > [style="display: inline-block; box-sizing: border-box; width: 140px;"]').contains('#text', 'Error');
+
+        
+
 
 
     });

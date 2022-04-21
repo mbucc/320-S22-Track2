7eb0cfe9a2a91cc7d9c207793c022ceb37f8b569
Emmie Ohnuki <emaohn@gmail.com>
2022-04-20 21:48:12 -0400

    fix lint errors

--- front-end/pages/login/Login.js
+++ front-end/pages/login/Login.js
@@ -1,42 +1,44 @@
-import React, { Component } from "react";
-import CustomInput from "./CustomInput";
-import Button from "./Buttons";
+import React from 'react';
+import CustomInput from './CustomInput';
+import Button from './Buttons';
 
-export default function Login ({setLogin}) {
+/**
+ * @param {Object} props
+* @return {JSX.Element}
+*/
+export default function Login({setLogin}) {
+  return (
+    <div className="App">
 
-    return (
-      <div className="App">
-        
-        
-      
-        <form className="form">
-          <CustomInput
-            labelText="Email"
-            id="email"
-            formControlProps={{
-              fullWidth: true
-            }}
-            // handleChange={this.handleChange}
-            type="text"
-          />
-          <CustomInput
-            labelText="Password"
-            id="password"
-            formControlProps={{
-              fullWidth: true
-            }}
-            // handleChange={this.handleChange}
-            type="password"
-          />
 
-          <Button type="button" color="primary" className="form__custom-button" onClick = {() => setLogin(true) }>
+      <form className="form">
+        <CustomInput
+          labelText="Email"
+          id="email"
+          formControlProps={{
+            fullWidth: true,
+          }}
+          // handleChange={this.handleChange}
+          type="text"
+        />
+        <CustomInput
+          labelText="Password"
+          id="password"
+          formControlProps={{
+            fullWidth: true,
+          }}
+          // handleChange={this.handleChange}
+          type="password"
+        />
+
+        <Button type="button" color="primary" className="form__custom-button" onClick = {() => setLogin(true) }>
             Log in
-          </Button>
-          <Button type="button" color="primary" className="form__custom-button" onClick = {() => setLogin(false) }>
+        </Button>
+        <Button type="button" color="primary" className="form__custom-button" onClick = {() => setLogin(false) }>
             Forgot Password
-          </Button>
-        </form>
-      </div>
-    );
-  }
+        </Button>
+      </form>
+    </div>
+  );
+}
 

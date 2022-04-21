32f222354824d36b4112a17d68c91fb41ac146a7
Erfan Fathalizadeh <efathalizade@umass.edu>
2022-04-21 01:59:13 -0400

    test update

--- front-end/cypress/integration/business-process/activity-table.spec.js
+++ front-end/cypress/integration/business-process/activity-table.spec.js
@@ -1,12 +1,18 @@
 // TODO: Write tests for activity table.
 
+//const { cy } = require("date-fns/locale");
+
+
+
+
+ 
+
+
 describe("Activity Table Behavior", () => {
     beforeEach(() => {
         cy.visit("/business-process");
     });    
     //checks if clicking component in tree corresponds to the activity showing up in the activity table
-
-    /*
     it("Populating activity table by clicking the tree", () => { 
         cy.get('#mui-1-EAI\\ Domain\\ 1 > .css-1g86id8-MuiTreeItem-content > .MuiTreeItem-label').click();
 
@@ -19,125 +25,174 @@ describe("Activity Table Behavior", () => {
         cy.get(':nth-child(1) > .tr > [style="display: inline-block; box-sizing: border-box; width: 140px;"]').should('exist');
 
     });
-    */
-    /*
     
-    it("Sort activities in ascending order (severities should be first)", () => {
-        cy.get('#mui-1-EAI\\ Domain\\ 1 > .css-1g86id8-MuiTreeItem-content > .MuiTreeItem-label').click();
+});
 
-        cy.get('#mui-1-Publishing\\ Business\\ Domain\\ 1\\ from\\ EAI\\ 1 > .css-1g86id8-MuiTreeItem-content > .MuiTreeItem-label').click();
-
-        cy.get('#mui-1-Business\\ Process\\ 1001 > .css-1g86id8-MuiTreeItem-content > .MuiTreeItem-label').click();
-
-        cy.get('#mui-1-1 > .css-1g86id8-MuiTreeItem-content > .MuiTreeItem-label').click(); 
-
-        cy.get('[style="display: inline-block; box-sizing: border-box; width: 140px; position: relative;"] > .table-header-sorter').click();
-
-        cy.get(':nth-child(1) > .tr > [style="display: inline-block; box-sizing: border-box; width: 140px;"]').contains('#text', 'Error');
-
-        
-
-
-
-    });
-    */
-
-    
-    
-
-    it('severity', () => {
-        
-        cy.get('#mui-1-EAI\\ Domain\\ 1 > .css-1g86id8-MuiTreeItem-content > .MuiTreeItem-label').click();
-
-        cy.get('#mui-1-Publishing\\ Business\\ Domain\\ 1\\ from\\ EAI\\ 1 > .css-1g86id8-MuiTreeItem-content > .MuiTreeItem-label').click();
-
-        cy.get('#mui-1-Business\\ Process\\ 1001 > .css-1g86id8-MuiTreeItem-content > .MuiTreeItem-label').click();
-
-        cy.get('#mui-1-1 > .css-1g86id8-MuiTreeItem-content > .MuiTreeItem-label').click(); 
-
-        cy.get('[style="display: inline-block; box-sizing: border-box; width: 140px; position: relative;"] > .table-header-sorter').click();
-
-        const correctSeverityAccending = ['Error', 'Warning', 'Info', 'Success']
-        const correctSeverityDecending = ['Success', 'Info', 'Warning', 'Warning']
-
-        cy.get('.tr > [style="display: inline-block; box-sizing: border-box; width: 140px;"]').should(($e) => {
-            const currentSeverityAccending = $e.toArray().map(el => el.innerText)
-            const arr = correctSeverityAccending.slice(correctSeverityAccending.length-currentSeverityAccending.length)
-            expect(currentSeverityAccending).to.deep.eq(arr)
-        });
-
-        cy.get('[style="display: inline-block; box-sizing: border-box; width: 140px; position: relative;"] > .table-header-sorter').click();
-
-        cy.get('.tr > [style="display: inline-block; box-sizing: border-box; width: 140px;"]').should(($e) => {
-            const currentSeverityDecending = $e.toArray().map(el => el.innerText)
-            const arr = correctSeverityDecending.slice(correctSeverityDecending.length-currentSeverityDecending.length)
-            expect(currentSeverityDecending).to.deep.eq(arr)
-        });
-         
-    });
-    
-
-    
-    var originalData = [[],[],[],[],[]]
-    var sortedAccending = [[],[],[],[],[]]
-    var sortedDecending = [[],[],[],[],[]]
-    it('copies', () => {
-        
-        cy.get('#mui-1-EAI\\ Domain\\ 1 > .css-1g86id8-MuiTreeItem-content > .MuiTreeItem-label').click();
-
-        cy.get('#mui-1-Publishing\\ Business\\ Domain\\ 1\\ from\\ EAI\\ 1 > .css-1g86id8-MuiTreeItem-content > .MuiTreeItem-label').click();
-
-        cy.get('#mui-1-Business\\ Process\\ 1001 > .css-1g86id8-MuiTreeItem-content > .MuiTreeItem-label').click();
-
-        cy.get('#mui-1-1 > .css-1g86id8-MuiTreeItem-content > .MuiTreeItem-label').click(); 
-
-        cy.get('.tr').each(($e, index) => {
-            if(index != 0){
-                cy.wrap($e).within(() => {
-                    cy.get('.td').each(($e2, index2) => {
-                        var text = $e2.text()
-                        originalData[index-1].push(text)
-                    })
+const tableToArray = (arr) => {
+    cy.get('.tr').each(($e, index) => {
+        if(index != 0){
+            cy.wrap($e).within(() => {
+                cy.get('.td').each(($e2, index2) => {
+                    var text = $e2.text()
+                    arr[index-1].push(text)
                 })
-            }
-        })
+            })
+        }
+    })
+}
+
+const initializeArray = () => {
+    originalData = [[], [], [], [], []]
+    sortedAccending = [[], [], [], [], []]
+    sortedDecending =  [[], [], [], [], []]
+    sortedUnordered = [[], [], [], [], []]
+}
+
+// flag indicates whether to sort in decs or asc order(true-> asc and false-> decs)
+// f is a function that determines
+// cI is the index of column to sort based on that
+const sortArray = (arr, flag, cI, t) => {
+    arr = arr.slice().sort((a, b) => {
+        let f = t(a[cI])
+        let s = t(b[cI])
+        return flag ? s-f : f-s
+    })
+    return arr; 
+}
+
+const arrayAreEqual = (arr1, arr2) => {
+    if(arr1.length != arr2.length){
+        expect(false).to.be.true
+    }
+    arr1.forEach(element => {
+        for(let i = 0; i < arr1.length; i++){
+            expect(arr1[i]).to.have.ordered.members(arr2[i])
+        }
+    });
+}
+
+
+var originalData = [[],[],[],[],[]]
+var sortedAccending = [[],[],[],[],[]]
+var sortedDecending =  [[],[],[],[],[]]
+var sortedUnordered = [[],[],[],[],[]] 
+describe("Table Sorts correctly based on date", () => {
+
+    beforeEach(() => {
+        cy.visit("/business-process");
+    });
+    
+    it('copy values', () => {
+        
+        cy.get('#mui-1-EAI\\ Domain\\ 1 > .css-1g86id8-MuiTreeItem-content > .MuiTreeItem-label').click();
+
+        cy.get('#mui-1-Publishing\\ Business\\ Domain\\ 1\\ from\\ EAI\\ 1 > .css-1g86id8-MuiTreeItem-content > .MuiTreeItem-label').click();
+
+        cy.get('#mui-1-Business\\ Process\\ 1001 > .css-1g86id8-MuiTreeItem-content > .MuiTreeItem-label').click();
+
+        cy.get('#mui-1-1 > .css-1g86id8-MuiTreeItem-content > .MuiTreeItem-label').click(); 
+
+        tableToArray(originalData);
 
         cy.get('[style="display: inline-block; box-sizing: border-box; width: 265px; position: relative;"] > .table-header-sorter').click()
 
-        cy.get('.tr').each(($e, index) => {
-            if(index != 0){
-                cy.wrap($e).within(() => {
-                    cy.get('.td').each(($e2, index2) => {
-                        var text = $e2.text()
-                        var arr = originalData[index-1].slice().sort((a, b) => b.date - a.date)
-                        expect(arr[index2]).to.equal(text)
-                        
-                        sortedAccending[index-1].push(text)
-                    })
-                })
-            }
-        })
+        tableToArray(sortedAccending);
 
         cy.get('[style="display: inline-block; box-sizing: border-box; width: 265px; position: relative;"] > .table-header-sorter').click()
 
-        cy.get('.tr').each(($e, index) => {
-            if(index != 0){
-                cy.wrap($e).within(() => {
-                    cy.get('.td').each(($e2, index2) => {
-                        var text = $e2.text()
-                        sortedDecending[index-1].push(text)
-                    })
-                })
-            }
-        })
+        tableToArray(sortedDecending);
+
+        cy.get('[style="display: inline-block; box-sizing: border-box; width: 265px; position: relative;"] > .table-header-sorter').click()
+
+        tableToArray(sortedUnordered);
+        
+
          
     });
 
-    
+    it('Date sorted in Accending order works correctly', () => {
+        var arr = sortArray(originalData, true, 1, (a) => new Date(a))
+        arrayAreEqual(arr, sortedAccending)
+    })
 
+    it('Date sorted in Decending order works correctly', () => {
+        var arr = sortArray(originalData, false, 1, (a) => new Date(a))
+        arrayAreEqual(arr, sortedDecending)
+    })
 
-      
+    it('Date unordered works correctly', () => {
+        arrayAreEqual(originalData, sortedUnordered)
+    })
 
 });
+
+
+describe("Table Sorts correctly based on severity", () => {
+    
+
+    beforeEach(() => {
+        cy.visit("/business-process");
+    });
+    
+    it('copy values', () => {
+        originalData = [[], [], [], [], []]
+        sortedAccending = [[], [], [], [], []]
+        sortedDecending =  [[], [], [], [], []]
+        sortedUnordered = [[], [], [], [], []]
+        
+        cy.get('#mui-1-EAI\\ Domain\\ 1 > .css-1g86id8-MuiTreeItem-content > .MuiTreeItem-label').click();
+
+        cy.get('#mui-1-Publishing\\ Business\\ Domain\\ 1\\ from\\ EAI\\ 1 > .css-1g86id8-MuiTreeItem-content > .MuiTreeItem-label').click();
+
+        cy.get('#mui-1-Business\\ Process\\ 1001 > .css-1g86id8-MuiTreeItem-content > .MuiTreeItem-label').click();
+
+        cy.get('#mui-1-1 > .css-1g86id8-MuiTreeItem-content > .MuiTreeItem-label').click(); 
+
+        tableToArray(originalData);
+
+        cy.get('[style="display: inline-block; box-sizing: border-box; width: 140px; position: relative;"] > .table-header-sorter').click()
+
+        tableToArray(sortedAccending);
+
+        cy.get('[style="display: inline-block; box-sizing: border-box; width: 140px; position: relative;"] > .table-header-sorter').click()
+
+        tableToArray(sortedDecending);
+
+        cy.get('[style="display: inline-block; box-sizing: border-box; width: 140px; position: relative;"] > .table-header-sorter').click()
+
+        tableToArray(sortedUnordered);
+        
+
+         
+    });
+
+    it('sevirity sorted in Accending order works correctly', () => {
+        
+        var arr = sortArray(originalData, true, 0, (a) => {
+            if(a == 'Error'){return 4}
+            if(a == 'Warning'){return 3}
+            if(a == 'Info'){return 2}
+            if(a == 'Success'){return 1}
+        })
+        arrayAreEqual(arr, sortedAccending)
+    })
+
+    it('sevirity sorted in Decending order works correctly', () => {
+        var arr = sortArray(originalData, false, 0, (a) => {
+            if(a == 'Error'){return 4}
+            if(a == 'Warning'){return 3}
+            if(a == 'Info'){return 2}
+            if(a == 'Success'){return 1}
+        })
+        arrayAreEqual(arr, sortedDecending)
+    })
+
+    it('sevirity unordered works correctly', () => {
+        arrayAreEqual(originalData, sortedUnordered)
+    })
+
+});
+
+
 
  

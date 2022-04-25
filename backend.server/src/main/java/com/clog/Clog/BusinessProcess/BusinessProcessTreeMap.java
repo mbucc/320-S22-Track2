c376af6bec1e3547a9642e2b1d910589a5b864b7
bmac400 <bryanmccaf@gmail.com>
2022-04-25 17:55:16 -0400

    Bug fix

--- backend.server/src/main/java/com/clog/Clog/BusinessProcess/BusinessProcessTreeMap.java
+++ backend.server/src/main/java/com/clog/Clog/BusinessProcess/BusinessProcessTreeMap.java
@@ -5,6 +5,8 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+
+
 public class BusinessProcessTreeMap {
     private Map<String, Map<String, Map<String, List<BusinessProcessTreeNode>>>> eai_domainTopLevel;
     public BusinessProcessTreeMap() {
@@ -39,11 +41,12 @@ public class BusinessProcessTreeMap {
             }
             //publishing business domain not in EAI map
             else {
+
                 Map<String, List<BusinessProcessTreeNode>> pubbusProc = new HashMap<String, List<BusinessProcessTreeNode>>();
                 List<BusinessProcessTreeNode> listToAdd = new ArrayList<BusinessProcessTreeNode>();
                 listToAdd.add(toAddNode);
                 pubbusProc.put(treeEntry.getBusiness_process(), listToAdd);
-                EaiMap.put(treeEntry.getEai_domain(), pubbusProc);
+                EaiMap.put(treeEntry.getPublishing_business_domain(), pubbusProc);
             }
 
         }

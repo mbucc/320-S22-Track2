2c3c4a5479ae6f5196b1e12c87b49b733989eb7c
Lingxi Li <lilingxi01@gmail.com>
2022-05-01 01:23:59 -0400

    [Cypress] Add mock data and a mock tree API.

---
+++ front-end/cypress/integration/business-process/mock-api/tree.js
@@ -0,0 +1,28 @@
+import TreeData from '../../../fixtures/business-process/data.json';
+
+/**
+ * This function is used to mock the api call for the tree
+ * @param {string[]} eaiDomains
+ * @param {string[]} pubDomains
+ * @return {object}
+ */
+function getTreeResult({eaiDomains = [], pubDomains = []}) {
+  const result = {};
+  const treeData = JSON.parse(JSON.stringify(TreeData));
+  treeData.forEach((eai) => {
+    if (eaiDomains.includes(eai.name) && eai.children) {
+      eai.children.forEach((pub) => {
+        if (pubDomains.includes(pub.name) && pub.children) {
+          result[eai.name] = result[eai.name] || {};
+          result[eai.name][pub.name] = {};
+          pub.children.forEach((bp) => {
+            if (bp.activities) {
+              result[eai.name][pub.name][bp.name] = bp.activities;
+            }
+          });
+        }
+      });
+    }
+  });
+  return result;
+}

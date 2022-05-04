38b8994e8ac42a88ba1e4b631d6bff7c592b8076
manodakshin24 <pdakshinamur@umass.edu>
2022-05-04 13:29:23 -0400

    started acceptance test

--- front-end/cypress/integration/log-events/acceptanceTest1.spec.js
+++ front-end/cypress/integration/log-events/acceptanceTest1.spec.js
@@ -1,19 +1,30 @@
 describe('testing functionality of login and log detail', ()=>{
-    before(()=>{
-        cy.visit('http://localhost:3000/');
-      });
+  before(()=>{
+    cy.visit('http://localhost:3000/');
+  });
 
-    it('visits main page', ()=>{
-        cy.visit('http://localhost:3000/');
-        cy.get('[role=option]:contains("Log Events")').click();
-    });
+  it('visits main page', ()=>{
+    cy.visit('http://localhost:3000/LogEvent');
+    cy.get(`[data-testid='button-login']`).click();
+  });
+});
 
-    });
+// it(){
 
-    //it(){
+// };
 
-    //};
+/*
+    describe('login works successfully', ()=>{
+        before(()=>{
+            cy.visit('http://localhost:3000/');
+          });
+
+        it('visits login', ()=>{
+            cy.get(`[data-testid='log-in']`)
+              .find(`[data-testid='log-in-error']`)
+              .click()
+        });
+    });*/
 
 
-
-//);
+// );

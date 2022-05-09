892857573f85965effb395e45cf9528cf63fd3d5
Lingxi Li <lilingxi01@gmail.com>
2022-05-08 23:58:02 -0400

    [Cypress] Fix mock tree API.

--- front-end/cypress/support/business-process/mock-api/tree.js
+++ front-end/cypress/support/business-process/mock-api/tree.js
@@ -1,42 +1,88 @@
-import TreeData from '../../../fixtures/business-process/data.json';
+// import TreeData from '../../../fixtures/business-process/data.json';
+import BusinessProcessTable from '../../../fixtures/business-process/business-process-table.json';
+import moment from 'moment';
 
 /**
  * This function is used to mock the api call for the tree
+ * @param {moment.Moment} startDate
+ * @param {moment.Moment} endDate
  * @param {string[]} eaiDomains
  * @param {string[]} pubDomains
  * @return {object}
  */
-function getTreeResult({eaiDomains = [], pubDomains = []}) {
+function getTreeResult(
+    {
+      startDate = moment().subtract(30, 'minutes'),
+      endDate = moment(),
+      eaiDomains = [],
+      pubDomains = [],
+    }
+) {
   const result = {};
-  const treeData = JSON.parse(JSON.stringify(TreeData));
-  treeData.forEach((eai) => {
-    if ((eaiDomains.length === 0 || eaiDomains.includes(eai.name)) && eai.children) {
-      eai.children.forEach((pub) => {
-        if ((pubDomains.length === 0 || pubDomains.includes(pub.name)) && pub.children) {
-          result[eai.name] = result[eai.name] || {};
-          result[eai.name][pub.name] = {};
-          pub.children.forEach((bp) => {
-            if (bp.activities) {
-              result[eai.name][pub.name][bp.name] = bp.activities;
-            }
-          });
-        }
-      });
+
+  // Make data into readable JSON object.
+  const bpTable = BusinessProcessTable;
+
+  bpTable.forEach((bp) => {
+    if (
+      bp.eai_domain &&
+      (eaiDomains.length === 0 || eaiDomains.includes(bp.eai_domain)) &&
+      bp.pub_domain &&
+      (pubDomains.length === 0 || pubDomains.includes(bp.pub_domain)) &&
+      moment(bp.eai_transaction_create_time).isBetween(startDate, endDate)
+    ) {
+      result[bp.eai_domain] = result[bp.eai_domain] || {};
+      result[bp.eai_domain][bp.pub_domain] = result[bp.eai_domain][bp.pub_domain] || {};
+      result[bp.eai_domain][bp.pub_domain][bp.business_process] =
+          result[bp.eai_domain][bp.pub_domain][bp.business_process] || [];
+      result[bp.eai_domain][bp.pub_domain][bp.business_process].push(bp);
     }
   });
+
+  // Below is the old mock data API function. Keep it for reference or rollback.
+  //
+  // const treeData = JSON.parse(JSON.stringify(TreeData));
+  // treeData.forEach((eai) => {
+  //   if ((eaiDomains.length === 0 || eaiDomains.includes(eai.name)) && eai.children) {
+  //     eai.children.forEach((pub) => {
+  //       if ((pubDomains.length === 0 || pubDomains.includes(pub.name)) && pub.children) {
+  //         result[eai.name] = result[eai.name] || {};
+  //         result[eai.name][pub.name] = {};
+  //         pub.children.forEach((bp) => {
+  //           if (bp.activities) {
+  //             result[eai.name][pub.name][bp.name] = bp.activities;
+  //           }
+  //         });
+  //       }
+  //     });
+  //   }
+  // });
   return result;
 }
+
 /**
  * This function is used to get EAI Domains
  * @return {string[]} eaiDomains
  */
 function getEAIDomain() {
-  const eaiDomains = [];
-  const treeData = JSON.parse(JSON.stringify(TreeData));
-  treeData.forEach((eai) => {
-    eaiDomains.push(eai.name);
+  const eaiDomains = new Set();
+
+  const bpTable = BusinessProcessTable;
+
+  bpTable.forEach((bp) => {
+    if (bp.eai_domain) {
+      eaiDomains.add(bp.eai_domain);
+    }
   });
-  return eaiDomains;
+
+  // Below is the old mock data API function. Keep it for reference or rollback.
+  //
+  // const treeData = JSON.parse(JSON.stringify(TreeData));
+  // treeData.forEach((eai) => {
+  //   eaiDomains.push(eai.name);
+  // });
+
+  return [...eaiDomains];
 }
 
 /**
@@ -44,16 +90,29 @@ function getEAIDomain() {
  * @return {string[]} pubDomains
  */
 function getPubDomain() {
-  const pubDomains = [];
-  const treeData = JSON.parse(JSON.stringify(TreeData));
-  treeData.forEach((eai) => {
-    if (eai.children) {
-      eai.children.forEach((pub) => {
-        pubDomains.push(pub.name);
-      });
+  const pubDomains = new Set();
+
+  const bpTable = BusinessProcessTable;
+
+  bpTable.forEach((bp) => {
+    if (bp.pub_domain) {
+      pubDomains.add(bp.pub_domain);
     }
   });
-  return pubDomains;
+
+  // Below is the old mock data API function. Keep it for reference or rollback.
+  //
+  // const pubDomains = [];
+  // const treeData = JSON.parse(JSON.stringify(TreeData));
+  // treeData.forEach((eai) => {
+  //   if (eai.children) {
+  //     eai.children.forEach((pub) => {
+  //       pubDomains.push(pub.name);
+  //     });
+  //   }
+  // });
+
+  return [...pubDomains];
 }
 
 export const BPTreeMockAPI = {

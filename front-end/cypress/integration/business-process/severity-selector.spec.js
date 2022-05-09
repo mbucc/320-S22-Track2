8b2c3c89b58623cd0973a9c911c4601f590c4fe5
TornadoOfSoup <senseipower@gmail.com>
2022-05-09 18:28:21 -0400

    Completed severity and domain selectors' testing, though the domain selector testing occasionally fails due to the test attempting to load text before the API loads it

--- front-end/cypress/integration/business-process/severity-selector.spec.js
+++ front-end/cypress/integration/business-process/severity-selector.spec.js
@@ -1,12 +1,91 @@
+import {goThroughLogin} from '../../support/business-process/utility/general';
 import { isValidTimeFormat } from "../../../utils/business-process/date-options";
+import { severityOptions, sortSeverityTags } from '../../../utils/business-process/severity';
 
 before(() => {
     cy.visit('/business-process');
     goThroughLogin();
-  });
+});
 
-  describe('Severity checkboxes and check/uncheck all buttons are working properly', () => {
-      it('Checkboxes can be checked and unchecked.', () => {
-          
-      })
-  })
+const clickSeverity = (option) => {
+    cy.get(`#bp-activity-filter-severity-selector-option-${option}`).click();
+};
+
+describe('Severity checkboxes and check/uncheck all buttons are working properly', () => {
+    it('Checkboxes can be unchecked and checked.', () => {
+        cy.wrap(Array.from(severityOptions)).each((option) => {
+            cy.get(`#bp-activity-filter-severity-selector-option-${option} > div > .icon`).should('exist');
+            clickSeverity(option);
+            cy.get(`#bp-activity-filter-severity-selector-option-${option} > div > .icon`).should('not.exist');
+        });
+
+        cy.wrap(Array.from(severityOptions)).each((option) => {
+            cy.get(`#bp-activity-filter-severity-selector-option-${option} > div > .icon`).should('not.exist');
+            clickSeverity(option);
+            cy.get(`#bp-activity-filter-severity-selector-option-${option} > div > .icon`).should('exist');
+        });
+    });
+
+    it('\"Check All\"/\"Uncheck All\" button works correctly.', () => {
+        cy.wrap(Array.from(severityOptions)).each((option) => {
+            cy.get(`#bp-activity-filter-severity-selector-option-${option} > div > .icon`).should('exist');
+        });
+
+        cy.get('.sc-eCYdqJ').click();
+
+        cy.wrap(Array.from(severityOptions)).each((option) => {
+            cy.get(`#bp-activity-filter-severity-selector-option-${option} > div > .icon`).should('not.exist');
+        });
+
+        cy.get('.sc-eCYdqJ').click();
+
+        cy.wrap(Array.from(severityOptions)).each((option) => {
+            cy.get(`#bp-activity-filter-severity-selector-option-${option} > div > .icon`).should('exist');
+        });
+
+        cy.get('.sc-eCYdqJ').click();
+        clickSeverity(severityOptions[0]);
+        cy.get('.sc-eCYdqJ').click();
+
+        cy.wrap(Array.from(severityOptions)).each((option) => {
+            cy.get(`#bp-activity-filter-severity-selector-option-${option} > div > .icon`).should('exist');
+        });
+
+        cy.wrap(Array.from(severityOptions)).each((option) => {
+            clickSeverity(option);
+        });
+        
+        cy.wrap(Array.from(severityOptions)).each((option) => {
+            cy.get(`#bp-activity-filter-severity-selector-option-${option} > div > .icon`).should('not.exist');
+        });
+
+        cy.wrap(Array.from(severityOptions)).each((option) => {
+            clickSeverity(option);
+        });
+        
+        cy.wrap(Array.from(severityOptions)).each((option) => {
+            cy.get(`#bp-activity-filter-severity-selector-option-${option} > div > .icon`).should('exist');
+        });
+
+        cy.get('.sc-eCYdqJ').click();
+        cy.wrap(Array.from(severityOptions)).each((option) => {
+            cy.get(`#bp-activity-filter-severity-selector-option-${option} > div > .icon`).should('not.exist');
+        });
+    });
+
+    it('Error state activates when there is no severity checked.', () => {
+        cy.wrap(Array.from(severityOptions)).each((option) => {
+            cy.get(`#bp-activity-filter-severity-selector-option-${option} > div > .icon`).should('not.exist');
+        });
+
+        cy.get('[style="display: flex; flex-direction: row; align-items: flex-start; justify-content: flex-start; width: 100%; padding-top: 4px; color: rgb(220, 38, 38);"]').should('exist');
+        clickSeverity(severityOptions[0]);
+        cy.get('[style="display: flex; flex-direction: row; align-items: flex-start; justify-content: flex-start; width: 100%; padding-top: 4px; color: rgb(220, 38, 38);"]').should('not.exist');
+        cy.get('.sc-eCYdqJ').click();
+        cy.get('[style="display: flex; flex-direction: row; align-items: flex-start; justify-content: flex-start; width: 100%; padding-top: 4px; color: rgb(220, 38, 38);"]').should('not.exist');
+        cy.get('.sc-eCYdqJ').click();
+        cy.get('[style="display: flex; flex-direction: row; align-items: flex-start; justify-content: flex-start; width: 100%; padding-top: 4px; color: rgb(220, 38, 38);"]').should('exist');
+    });
+
+
+});

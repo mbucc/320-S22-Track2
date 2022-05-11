650b2425e81b75b73f42f5076e095d43035439a6
limbo-yan <46770483+limbo-yan@users.noreply.github.com>
2022-05-10 22:37:11 -0400

    [Acceptance Test] Datepicker check activities 2.0

--- front-end/cypress/integration/business-process/acceptance/acceptance-date-picker.spec.js
+++ front-end/cypress/integration/business-process/acceptance/acceptance-date-picker.spec.js
@@ -5,14 +5,16 @@ Ben from the maintenance team found an unknown issue reported on 05/01/2022 afte
 
 import {goThroughLogin} from '../../../support/business-process/utility/general';
 import moment from 'moment';
+import {selectTreeItem} from '../../../support/business-process/input/tree-selection';
 import {inputEndDate, inputStartDate} from '../../../support/business-process/input/date-picker';
 import {
   interceptBusinessDomainList,
   interceptGridAPI,
   interceptTreeAPI,
 } from '../../../support/business-process/utility/intercept';
-import {clickTreeApplyButton} from '../../../support/business-process/input/apply-button';
+import {clickTableApplyButton, clickTreeApplyButton} from '../../../support/business-process/input/apply-button';
 
+const testingEAITransactionId = 'eai-trans-id-bAXGNC-039696';
 const testingTime = '2022-05-01T06:00:00+00:00';
 
 // Call before every test to prepare the environment.
@@ -33,7 +35,7 @@ describe('Acceptance Test of User 1', () => {
   const currentTime = moment(testingTime);
   const past1Hour = currentTime.clone().subtract(1, 'hours');
 
-  it('Test Functionality of DatePicker', () => {
+  it('DatePicker should show business processes correctly', () => {
     interceptTreeAPI({
       startDate: past1Hour,
       endDate: currentTime,
@@ -42,6 +44,26 @@ describe('Acceptance Test of User 1', () => {
     getStartDateByPopper();
     inputEndDate('+1h');
     clickTreeApplyButton();
-    cy.get('#expand-collapse-all-button').click();
+    interceptGridAPI({
+      eaiTransactionId: testingEAITransactionId,
+    }).as('getGrid');
+
+    selectTreeItem(testingEAITransactionId);
+    cy.wait('@getGrid');
+
+    interceptBusinessDomainList();
+  });
+
+  it('Business processes with errors would only be shown after applying severity filter', () => {
+    cy.get('.sc-eCYdqJ').click();
+
+    // Filter the error activities
+    cy.get(`#bp-activity-filter-severity-selector-option-error`).click();
+    interceptGridAPI({
+      eaiTransactionId: testingEAITransactionId,
+      severities: ['error'],
+    }).as('getGrid');
+    clickTableApplyButton();
+    cy.wait('@getGrid');
   });
 });

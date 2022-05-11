270daee7383ef0720b83a891663634704730bc3f
Aishwarya Rajapur <arjun.aishwarya@gmail.com>
2022-05-11 13:14:47 -0400

    intercept EAI domain list

--- front-end/cypress/integration/business-process/acceptance/acceptance-domain-selector.spec.js
+++ front-end/cypress/integration/business-process/acceptance/acceptance-domain-selector.spec.js
@@ -3,35 +3,58 @@ ticket for incorrect Accounting information at 9AM today.*/
 
 import {goThroughLogin} from '../../../support/business-process/utility/general';
 import moment from 'moment';
-import {selectSpecificPubDomain} from '.../.../.../support/business-process/input/domain-selection'
 import {inputEndDate, inputStartDate} from '../../../support/business-process/input/date-picker';
 import {
   interceptBusinessDomainList,
+  interceptEAIDomainList,
   interceptGridAPI,
   interceptTreeAPI,
 } from '../../../support/business-process/utility/intercept';
-import {clickTreeApplyButton} from '../../../support/business-process/input/apply-button';
+import {clickTableApplyButton, clickTreeApplyButton} from '../../../support/business-process/input/apply-button';
+import { selectSpecificEAIDomain } from '../../../support/business-process/input/domain-selection';
 
 
-const startDate = '5/9/2022, 9 AM';
-const endDate = '5/9/2022 9:10 AM'
 
-before(() => {
+import {interceptActivityFilter} from '../../../support/business-process/utility/intercept';
+import {domainSelection} from '../../../support/business-process/input/domain-selection';
+import {generatePath} from '../../../support/business-process/utility/path-generator';
+import {convertToAPIFormat} from '../../../../utils/business-process/date-options';
+import {BPTreeMockAPI} from '../../../support/business-process/mock-api/tree';
+import {selectTreeItem} from '../../../support/business-process/input/tree-selection';
+
+const testingTime = '2022-05-09T02:10:32+00:00';
+const testingEAITransactionId = 'eai-trans-id-rgewll-158495';
+
+const prepare = () => {
+  before(() => {
+    const currentTime = moment(testingTime);
     cy.visit('/business-process');
-    cy.clock(moment(startDate).utc().toDate().getTime());
+    cy.clock(moment(testingTime).utc().toDate().getTime());
     goThroughLogin();
-  });
-
-describe('Select accounting from publishing business domain', () => {
-    const targetTime = moment(startDate); //9 am 
-    const endTime = moment(endDate)
-    it("Inputs proper start and end time and publishing business domain", () => {
-        interceptTreeAPI({
-            startDate : targetTime,
-            endDate : endTime
-        }).as('getTree');
-    cy.get('#bp-tree-filter-publishing-business-domain-selector').click();
-    selectSpecificPubDomain('accounting_app');
-    clickTreeApplyButton();
-    });
+    interceptEAIDomainList(); //intercept first
 });
+};
+
+describe('Acceptance Test of User 2', () => {
+    prepare();
+    const current = moment(testingTime);
+    const past10 = current.clone().subtract(10, 'minutes');
+
+
+  it('Test Domain Selector', () => {
+
+    interceptTreeAPI({
+        startDate: past10,
+        endDate: current,
+        eaiDomains: ['EAI Thompson 9239']
+      }).as('getTree');
+      selectSpecificEAIDomain('EAI Thompson 9239');
+      inputStartDate(past10.format('MM/DD/YYYY HH:mm:ss A'));
+      inputEndDate(current.format('MM/DD/YYYY HH:mm:ss A'));
+
+    clickTreeApplyButton();
+  });
+});
+
+
+
